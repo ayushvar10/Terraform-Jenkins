f46325c83afe27b34167280fc0684bb381d478ef
Ayush Varshney <70069488+ayushvar10@users.noreply.github.com>
2022-05-31 18:02:24 +0530

    ec2 main file updated
    ec2 main file updated

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-ec2" {
   ami                    = data.aws_ami.amz2.id
   instance_type          = var.instance_type
-  key_name               = var.instance_key
+  #key_name               = var.instance_key
   user_data              = file("${path.module}/app1-install.sh")
   vpc_security_group_ids = [aws_security_group.ssh-sg.id, aws_security_group.web-sg.id]
   tags = {
